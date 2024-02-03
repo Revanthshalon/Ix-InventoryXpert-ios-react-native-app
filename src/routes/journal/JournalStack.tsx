26209914d5a10c8b37a3e537bb48bc8a30d75a42
S Revanth Shalon Raj <revanthshalon@gmail.com>
2024-02-03 21:14:17 +0530

    Add PurchaseForm screen to JournalStack

--- src/routes/journal/JournalStack.tsx
+++ src/routes/journal/JournalStack.tsx
@@ -3,11 +3,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import JournalOverviewScreen from "../../screens/journal/JournalOverviewScreen";
 import CompanyForm from "../../screens/company/CompanyForm";
 import PaymentForm from "../../screens/payments/PaymentForm";
+import PurchaseForm from "../../screens/purchases/PurchaseForm";
 
 export type JournalStackParamList = {
   Overview: undefined;
   CompanyForm: { formType: "add" } | { formType: "edit"; companyId: number };
   PaymentForm: { formType: "add" } | { formType: "edit"; paymentId: number };
+  PurchaseForm: { formType: "add" } | { formType: "edit"; purchaseId: number };
 };
 
 const JournalStack = () => {
@@ -20,6 +22,7 @@ const JournalStack = () => {
       <Stack.Screen name="Overview" component={JournalOverviewScreen} />
       <Stack.Screen name="CompanyForm" component={CompanyForm} />
       <Stack.Screen name="PaymentForm" component={PaymentForm} />
+      <Stack.Screen name="PurchaseForm" component={PurchaseForm} />
     </Stack.Navigator>
   );
 };
