408f5310015ec9faf5fc7a0c698891baf55684ff
S Revanth Shalon Raj <revanthshalon@gmail.com>
2024-02-03 18:53:59 +0530

    Add payment reducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import companyReducer from "./company/CompanySlice";
+import paymentReducer from "./payment/paymentSlice";
 
 export const store = configureStore({
   reducer: {
     company: companyReducer,
+    payment: paymentReducer,
   },
 });
 
