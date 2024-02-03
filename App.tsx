6961ba9d1c2d96c42d92e61eb9c452e5e3cbe920
S Revanth Shalon Raj <revanthshalon@gmail.com>
2024-02-03 12:52:07 +0530

    Refactor App component to use navigation and context providers

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import RootLayout from "./src/layouts/RootLayout";
+import DbContext from "./src/context/DbContext";
+import { openDatabaseSync } from "expo-sqlite/next";
+import { drizzle } from "drizzle-orm/expo-sqlite";
 
-export default function App() {
+/**
+ * The main component of the application.
+ * It initializes the database and provides the database context to the child components.
+ */
+const App = () => {
+  /**
+   * The database context.
+   */
+  const expoDb = openDatabaseSync("inventoryXpert.db");
+  const db = drizzle(expoDb);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <DbContext.Provider value={db}>
+            <RootLayout />
+          </DbContext.Provider>
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
