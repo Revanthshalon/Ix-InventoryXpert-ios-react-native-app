97d0548e68d1eb3a6a68e3bcead4ebf6bc316ee4
S Revanth Shalon Raj <revanthshalon@gmail.com>
2024-02-03 19:24:22 +0530

    Add payment form functionality

--- src/screens/payments/PaymentForm.tsx
+++ src/screens/payments/PaymentForm.tsx
@@ -1,17 +1,23 @@
 import {
   Keyboard,
+  NativeSyntheticEvent,
   StyleSheet,
-  Text,
+  TextInputChangeEventData,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
 import React, { useContext } from "react";
-import { Appbar, useTheme } from "react-native-paper";
+import { Appbar, Switch, Text, TextInput, useTheme } from "react-native-paper";
 import { JournalStackParamList } from "../../routes/journal/JournalStack";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import DbContext from "../../context/DbContext";
 import { Payment, getPaymentById } from "../../db/paymentSchema";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store";
+import DropDown from "react-native-paper-dropdown";
+import { DatePickerInput } from "react-native-paper-dates";
+import { addNewPayment } from "../../redux/payment/paymentSlice";
 
 type JournalNavigationProp = NativeStackNavigationProp<
   JournalStackParamList,
@@ -32,21 +38,42 @@ const PaymentForm = () => {
   const [paymentDetails, setPaymentDetails] = React.useState<
     Payment | undefined
   >(undefined);
+  const [showDropDown, setShowDropDown] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
-  React.useEffect(() => {
-    const getPaymentDetails = async () => {
-      if (paymentId) {
-        const response = await getPaymentById(db!, paymentId);
-        setPaymentDetails(response);
-      }
-    };
-    getPaymentDetails();
-  }, []);
+  const paymentsData = useSelector((state: RootState) => state.payment);
+  const companiesData = useSelector((state: RootState) => state.company);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const companiesList = companiesData.companies.map((company) => ({
+    label: company.name,
+    value: company.id,
+  }));
+
+  if (paymentId!) {
+    const payment = paymentsData.payments.find((p) => p.id === paymentId);
+    if (payment) {
+      setPaymentDetails(payment);
+    }
+  }
 
   const goBackHandler = () => {
     setPaymentDetails(undefined);
     JournalNav.goBack();
   };
+
+  const savePaymentHandler = async () => {
+    if (formType === "edit") {
+    }
+    if (formType === "add") {
+      dispatch(addNewPayment({ db: db!, payment: paymentDetails! }));
+      paymentsData.status === "loading" ? setLoading(true) : setLoading(false);
+      goBackHandler();
+      paymentsData.status = "idle";
+    }
+  };
+
+  console.log(paymentsData.payments);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -59,8 +86,92 @@ const PaymentForm = () => {
         <Appbar.Header mode="small" elevated>
           <Appbar.BackAction onPress={goBackHandler} />
           <Appbar.Content title="Payment Form" />
-          <Appbar.Action icon="content-save" onPress={() => {}} />
+          <Appbar.Action icon="content-save" onPress={savePaymentHandler} />
         </Appbar.Header>
+        <View style={[styles.formContainer]}>
+          <DropDown
+            mode="outlined"
+            label={"Company"}
+            visible={showDropDown}
+            showDropDown={() => {
+              setShowDropDown(true);
+            }}
+            onDismiss={() => setShowDropDown(false)}
+            value={paymentDetails?.companyId}
+            setValue={(value) => {
+              setPaymentDetails({ ...paymentDetails!, companyId: value });
+            }}
+            list={companiesList}
+            dropDownItemTextStyle={{
+              color: useTheme().colors.onBackground,
+            }}
+          />
+          <TextInput
+            mode="outlined"
+            label="Amount"
+            placeholder="Enter Amount"
+            value={paymentDetails?.amount?.toString()}
+            onBlur={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+              setPaymentDetails({
+                ...paymentDetails!,
+                amount: parseFloat(e.nativeEvent.text),
+              });
+            }}
+          />
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 30,
+              marginBottom: 30,
+            }}
+          >
+            <DatePickerInput
+              mode="outlined"
+              locale="en-GB"
+              label="Payment Date"
+              value={paymentDetails?.date}
+              onChange={(d) => {
+                setPaymentDetails({ ...paymentDetails!, date: d! });
+              }}
+              inputMode="start"
+            />
+          </View>
+          <TextInput
+            label="Remarks"
+            mode="outlined"
+            multiline
+            value={paymentDetails?.remarks?.toString()}
+            onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+              setPaymentDetails({
+                ...paymentDetails!,
+                remarks: e.nativeEvent.text,
+              });
+            }}
+          />
+          <View
+            style={[
+              {
+                flexDirection: "row",
+                marginVertical: 20,
+                marginHorizontal: 10,
+                alignItems: "center",
+                columnGap: 10,
+              },
+            ]}
+          >
+            <Text variant="titleSmall">Payment Status </Text>
+            <Switch
+              value={paymentDetails?.paymentStatus === "paid" ? true : false}
+              onValueChange={(value) => {
+                setPaymentDetails({
+                  ...paymentDetails!,
+                  paymentStatus: value ? "paid" : "pending",
+                });
+              }}
+            />
+          </View>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -71,5 +182,19 @@ export default PaymentForm;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  formContainer: {
+    flex: 1,
+    padding: 10,
+    rowGap: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    columnGap: 10,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    flexGrow: 1,
   },
 });
